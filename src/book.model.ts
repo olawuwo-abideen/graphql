ca8d88ad1dbb3e9b50ebe07237379722be97d9ca
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-03-24 23:50:34 +0100

    add appmodule

---
+++ src/book.model.ts
@@ -0,0 +1,21 @@
+// book.model.ts
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type BookDocument = Book & Document;
+
+@ObjectType()
+@Schema()
+export class Book {
+    @Field()
+    title: string;
+
+    @Field()
+    author: string;
+
+    @Field()
+    publishedDate: boolean;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
